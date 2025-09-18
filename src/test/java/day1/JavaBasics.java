5cb286f8fa8367e0da3b1b95618553aa9803c4e7
akshayburli27-eng <akshay.burli27@gmail.com>
2025-09-18 19:14:31 +0530

    commit

--- src/test/java/day1/JavaBasics.java
+++ src/test/java/day1/JavaBasics.java
@@ -3,6 +3,7 @@ package day1;
 public class JavaBasics {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
        // System.out.println("Hello Architha");
        /* System.out.println("Akshay");
        System.out.println("Hello world")
